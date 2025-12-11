95f704d4f13e7947d3355057170cce81049bed12
saeedkamyab <saeed.kamyab@yahoo.com>
2025-12-11 14:33:09 +0330

    Add project files.

---
+++ AdinaBlazorDataTable/Models/DataTableColumn.cs
@@ -0,0 +1,23 @@
+﻿public class DataTableColumn<TItem>
+{
+    public string Header { get; set; }
+    public string Field { get; set; }
+
+    public object GetValue(TItem item)
+    {
+        // با Reflection مقدار پراپرتی رو می‌گیریم
+        var propertyInfo = typeof(TItem).GetProperty(Field);
+        if (propertyInfo != null)
+        {
+            return propertyInfo.GetValue(item);
+        }
+        return string.Empty;
+    }
+
+    // Constructor ساده
+    public DataTableColumn(string header, string field)
+    {
+        Header = header;
+        Field = field;
+    }
+}
